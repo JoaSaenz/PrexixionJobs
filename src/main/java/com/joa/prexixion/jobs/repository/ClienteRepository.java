46919d7840aa54515379e7ce245c51def8970b44
Joaquin <joacotse@gmail.com>
2026-06-17 17:38:57 -0500

    refactor: remove unused method and include additional RUC in test query

--- src/main/java/com/joa/prexixion/jobs/repository/ClienteRepository.java
+++ src/main/java/com/joa/prexixion/jobs/repository/ClienteRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.joa.prexixion.jobs.model.Cliente;
@@ -14,14 +13,11 @@ public interface ClienteRepository extends JpaRepository<Cliente, String> {
     @Query(value = "SELECT c.ruc, c.y, c.solU, c.solC FROM cliente c WHERE c.idEstado IN (1,3,5,6,7,8,9) ORDER BY c.y ASC", nativeQuery = true)
     List<Cliente> obtenerClientes();
 
-    @Query(value = "SELECT c.ruc, c.solU, c.solC FROM cliente c LEFT JOIN signerNiveles n ON c.ruc = n.idCliente WHERE c.idEstado = :idEstado and c.y = :y and n.idNivelF = 4 and n.idNivelX3 = 5", nativeQuery = true)
-    List<Cliente> obtenerClientesActivos(@Param("idEstado") Integer idEstado, @Param("y") Integer y);
-
     @Query(value = """
             SELECT c.ruc, c.solU, c.solC, c.y
             FROM cliente c
             WHERE c.ruc IN (
-                '10188990920'
+                '10188990920', '20560043270'
             )
             """, nativeQuery = true)
     List<Cliente> obtenerClientesTest();
